939b9b93c830117a6fc100ff91840a34d785137b
Khanya Keswa <khanya@Khanyas-MacBook-Pro-2.local>
2020-02-17 18:27:55 -0600

    b4 ACA

---
+++ intro_to_vue.js/lesson_2/challenge_2/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'A pair of warm fuzzy socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+    altText: "A pair of socks",
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+  }
+})
---
+++ intro_to_vue.js/lesson_2/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'A pair of warm fuzzy socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+    altText: "A pair of socks"
+  }
+})
